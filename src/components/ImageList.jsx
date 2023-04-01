3de1573332ad44078b0ec623ce7ef4aa416f5af2
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 16:51:55 +0530

    fixed margin no image found

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -12,10 +12,12 @@ const ImageList = ({ imageSearch }) => {
       <h1 className=" text-center mt-6 text-2xl underline">
         Showing Results for {imageSearch}
       </h1>
+      {!loading && response && response.length === 0 && (
+        <div className="text-center text-2xl font-bold mt-5">
+          No images found
+        </div>
+      )}
       <div className=" max-w-7xl mx-auto px-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
-        {!loading && response && response.length === 0 && (
-          <div className="text-center text-2xl font-bold">No images found</div>
-        )}
         {loading ? (
           <Skeleton items={10} />
         ) : (
