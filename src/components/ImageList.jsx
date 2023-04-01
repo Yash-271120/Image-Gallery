604021a7e9629efbc2e237d466329bb1e80a8afa
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 16:34:58 +0530

    handled no images found

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -7,12 +7,16 @@ import { ImageContext } from "../App";
 
 const ImageList = ({ imageSearch }) => {
   const { response, loading } = useContext(ImageContext);
+  console.log(response);
   return (
     <>
       <h1 className=" text-center mt-6 text-2xl underline">
         Showing Results for {imageSearch}
       </h1>
       <div className=" max-w-7xl mx-auto px-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+        {response.length === 0 && !loading && (
+          <div className="text-center text-2xl font-bold">No images found</div>
+        )}
         {loading ? (
           <Skeleton items={10} />
         ) : (
