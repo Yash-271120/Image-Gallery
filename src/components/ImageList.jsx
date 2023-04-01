439be6d80ca5bf3743f1c0122e9677e40239db4d
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 16:42:05 +0530

    small fix

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -7,13 +7,14 @@ import { ImageContext } from "../App";
 
 const ImageList = ({ imageSearch }) => {
   const { response, loading } = useContext(ImageContext);
+  console.log(response);
   return (
     <>
       <h1 className=" text-center mt-6 text-2xl underline">
         Showing Results for {imageSearch}
       </h1>
       <div className=" max-w-7xl mx-auto px-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
-        {!loading && response.length === 0 && (
+        {!loading && response && response.length === 0 && (
           <div className="text-center text-2xl font-bold">No images found</div>
         )}
         {loading ? (
