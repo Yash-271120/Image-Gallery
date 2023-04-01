a99d87674d541537ea50a7c6a7791908f79566f0
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 16:18:28 +0530

    capitalized the first later of name

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -9,7 +9,10 @@ const Image = ({ data }) => {
         alt={data.alt_description}
       />
       <div className=" text-center mt-1">
-        <p className="mb-5 text-2xl">{data.alt_description}</p>
+        <p className="mb-5 text-2xl">
+          {data.alt_description.charAt(0).toUpperCase() +
+            data.alt_description.slice(1)}
+        </p>
         <div className=" h-10 w-full"></div>
       </div>
       <a
