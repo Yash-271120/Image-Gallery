94bee41228526d2ae42800da9a8881b61dd07bcf
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 16:14:44 +0530

    fixed visit button to bottom right

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 const Image = ({ data }) => {
   return (
-    <div className=" border-2 shadow-sm border-orange-400 rounded-lg p-4">
+    <div className=" border-2 shadow-sm border-orange-400 rounded-lg p-4 relative">
       <img
-        className=" h-64 w-full object-cover rounded-lg shadow-md"
+        className=" h-72 w-full object-cover rounded-lg shadow-md"
         src={data.urls.small}
         alt={data.alt_description}
       />
-      <div className=" text-center mt-5">
+      <div className=" text-center mt-1">
         <p className="mb-5 text-2xl">{data.alt_description}</p>
-        <a
-          className="bg-orange-400 p-3 rounded-lg text-white"
-          href={data.links.html}
-          target="_blank"
-        >
-          Visit page ↗
-        </a>
+        <div className=" h-10 w-full"></div>
       </div>
+      <a
+        className="bg-orange-400 p-3 rounded-lg text-white absolute bottom-0 right-0 mb-1 mr-1"
+        href={data.links.html}
+        target="_blank"
+      >
+        Visit page ↗
+      </a>
     </div>
   );
 };
