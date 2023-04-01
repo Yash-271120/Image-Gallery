7d5e5385279442fb01d6d940888f48965d13917f
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 11:20:18 +0530

    Image context set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import Jumbotron from "./utils/Jumbotron"
-import SearchField from "./utils/SearchField"
+import { createContext } from "react";
 
+import Jumbotron from "./utils/Jumbotron";
+import SearchField from "./utils/SearchField";
+import ImageList from "./components/ImageList";
+
+import useUnsplashApi from "./hooks/useUnsplashApi";
+
+export const ImageContext = createContext();
 
 function App() {
+  const { response, error, loading, fetchPhotos } = useUnsplashApi("puppies");
 
+  const data = {
+    response,
+    error,
+    loading,
+    fetchPhotos,
+  };
   return (
-    <>
-      <h1 className=' text-cyan-300 text-4xl'>React App</h1>
+    <ImageContext.Provider value={data}>
+      <h1 className=" text-cyan-300 text-4xl">React App</h1>
       <Jumbotron>
         <SearchField />
       </Jumbotron>
-    </>
-  )
+      <ImageList />
+    </ImageContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Image from "./Image";
 
+import { ImageContext } from "../App";
+
 const ImageList = () => {
+  const { response, loading } = useContext(ImageContext);
+  console.log(response);
   return (
     <>
       <h1 className=" text-center mt-6 text-2xl underline">
