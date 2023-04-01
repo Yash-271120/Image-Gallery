0c29bc91e2ff957336a92f33f11640f795f2ce52
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 11:12:02 +0530

    made ImageList component responsive

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import Image from "./Image";
 
 const ImageList = () => {
   return (
-    <div>ImageList</div>
-  )
-}
+    <>
+      <h1 className=" text-center mt-6 text-2xl underline">
+        Showing Results for Puppies
+      </h1>
+      <div className=" grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+      </div>
+    </>
+  );
+};
 
-export default ImageList
+export default ImageList;
