1e14ba58e5d01685e5a3514f3d9d687d6233e3e9
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 11:05:23 +0530

    Added unsplash API hook

---
+++ src/hooks/useUnsplashAPI.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+import { createApi } from "unsplash-js";
+
+const api = createApi({
+  accessKey: import.meta.env.VITE_ACCESS_KEY,
+});
+
+const useUnsplashApi = (query) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPhotos = async (query) => {
+    try {
+      setLoading(true);
+      const response = await api.search.getPhotos({
+        query: query,
+      });
+      setResponse(response.response.results);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos(query);
+  }, [query]);
+
+  return {
+    response,
+    error,
+    loading,
+    fetchPhotos,
+  };
+};
+
+export default useUnsplashApi;
