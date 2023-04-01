2e5c196bc39621e59cbf39f8da48feeae2f5dea4
Yash Patil <yashpatil.yp2711@gmail.com>
2023-04-01 14:29:25 +0530

    Added responsive grid of images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Image = () => {
+const Image = ({ data }) => {
   return (
-    <div>Image</div>
-  )
-}
+    <div className=" border-2 shadow-sm border-orange-400 rounded-lg p-4">
+      <h1>Image</h1>
+      <img
+        className=" h-64 w-full object-cover rounded-lg shadow-md"
+        src={data.urls.small}
+        alt={data.alt_description}
+      />
+      <div className=" text-center mt-5">
+        <p className="mb-5 text-2xl">{data.alt_description}</p>
+        <a href={data.links.html} target="_blank">
+          Link to Unsplash page
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Image
+export default Image;
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 
 import Image from "./Image";
+import Skeleton from "../utils/Skeleton";
 
 import { ImageContext } from "../App";
 
@@ -12,14 +13,15 @@ const ImageList = () => {
       <h1 className=" text-center mt-6 text-2xl underline">
         Showing Results for Puppies
       </h1>
-      <div className=" grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
-        <Image />
-        <Image />
-        <Image />
-        <Image />
-        <Image />
-        <Image />
-        <Image />
+      <div className=" max-w-7xl mx-auto px-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+        {loading ? (
+          <Skeleton items={10} />
+        ) : (
+          response &&
+          response.map((image, index) => {
+            return <Image key={index} data={image} />;
+          })
+        )}
       </div>
     </>
   );
